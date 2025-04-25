0b1e15fe1fb56cd6708066dc45478db9f508a518
jma <58450502+Y0URD34TH@users.noreply.github.com>
2025-04-25 03:47:25 -0300

    Update +page.svelte

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -6,6 +6,7 @@
 	let background: any = $state(undefined);
 	let backgroundUrl: any = $state('');
 	let previewUrl: string = $state('');
+
 	//@ts-ignore
 	let dialog: HTMLDialogElement = $state();
 
@@ -37,67 +38,49 @@
 		dialog.close();
 	}
 
-	function handleSubmit(event: SubmitEvent) {
-	// Let the form submit first (no preventDefault)
-	// Schedule the dummy fetch slightly after
-	setTimeout(() => {
-		fetch('/update', { method: 'GET' })
-			.then(() => console.log('Dummy /update request sent'))
-			.catch((err) => console.error('Dummy /update request failed', err));
-	}, 100);
-}
-
+	function onSaveComplete() {
+		console.log("Save completed. Triggering update.");
+		fetch('/update') // Adjust this path to match your internal update endpoint
+			.then(() => console.log('Update request sent.'))
+			.catch((err) => console.error('Update request failed', err));
+	}
 </script>
 
 <div class="settings">
-<form on:submit={handleSubmit} method="POST" action="?/save" enctype="multipart/form-data">
+	<iframe name="save-complete-frame" style="display:none;" onload={onSaveComplete}></iframe>
+
+	<form method="POST" action="?/save" enctype="multipart/form-data" target="save-complete-frame">
 		<h1>Settings:</h1>
 
 		<div class="images">
 			<div class="pfp-upload">
 				<div class="picture">
 					<img
-						src={previewUrl || data.pfp || `https://api.dicebear.com/9.x/identicon/svg?seed=${data.user.username}&backgroundColor=ffdfbf,b6e3f4`}
+						src={previewUrl ||
+							data.pfp ||
+							`https://api.dicebear.com/9.x/identicon/svg?seed=${data.user.username}&backgroundColor=ffdfbf,b6e3f4`}
 						alt="Preview"
 					/>
 				</div>
 				<label for="picture" class="custom-file-upload">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512">
-						<path
-							d="M288 109.3L288 352c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-242.7-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352l128 0c0 35.3 28.7 64 64 64s64-28.7 64-64l128 0c35.3 0 64 28.7 64 64l0 32c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64l0-32c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"
-						/>
-					</svg>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512"><path d="M288 109.3L288 352c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-242.7-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352l128 0c0 35.3 28.7 64 64 64s64-28.7 64-64l128 0c35.3 0 64 28.7 64 64l0 32c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64l0-32c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg>
 				</label>
-				<input
-					type="file"
-					id="picture"
-					name="picture"
-					accept="image/png, image/jpeg, image/webp"
-					onchange={handlePFPChange}
-				/>
+				<input type="file" id="picture" name="picture" accept="image/png, image/jpeg, image/webp" onchange={handlePFPChange} />
 			</div>
 
 			<div class="bg-upload">
 				<div class="background">
 					<img
-						src={backgroundUrl || data.bg || `https://api.dicebear.com/9.x/identicon/svg?seed=${data.user.username}&backgroundColor=ffdfbf,b6e3f4`}
+						src={backgroundUrl ||
+							data.bg ||
+							`https://api.dicebear.com/9.x/identicon/svg?seed=${data.user.username}&backgroundColor=ffdfbf,b6e3f4`}
 						alt="Preview"
 					/>
 				</div>
 				<label for="background" class="custom-file-upload">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512">
-						<path
-							d="M288 109.3L288 352c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-242.7-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352l128 0c0 35.3 28.7 64 64 64s64-28.7 64-64l128 0c35.3 0 64 28.7 64 64l0 32c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64l0-32c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"
-						/>
-					</svg>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512"><path d="M288 109.3L288 352c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-242.7-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352l128 0c0 35.3 28.7 64 64 64s64-28.7 64-64l128 0c35.3 0 64 28.7 64 64l0 32c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64l0-32c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg>
 				</label>
-				<input
-					type="file"
-					id="background"
-					name="background"
-					accept="image/png, image/jpeg, image/webp"
-					onchange={handleBGChange}
-				/>
+				<input type="file" id="background" name="background" accept="image/png, image/jpeg, image/webp" onchange={handleBGChange} />
 			</div>
 		</div>
 
@@ -112,6 +95,7 @@
 				<label for="passwordConfirm">Password confirm:</label>
 				<input type="password" name="passwordConfirm" />
 			</div>
+
 			<div class="right">
 				<div class="desc">
 					<label for="description">Description:</label>
@@ -145,12 +129,8 @@
 	<dialog class="delete" bind:this={dialog}>
 		<form action="/api/delete-account" method="POST">
 			<h1>Are you sure you want to delete your account?</h1>
-			<p>
-				This action is <b>Permanent</b> and <b>Irreversible</b>, you will lose all access to your
-				account and all data associated with it will be <b><u>deleted</u></b>.
-			</p>
+			<p>This action is <b>Permanent</b> and <b>Irreversible</b>, you will lose all access to your account and all data associated with it will be <b><u>deleted</u></b>.</p>
 			<p>Are you sure you want to proceed?</p>
-
 			<div class="btns">
 				<input type="submit" value="Delete account" />
 				<button onclick={closeModal}> Cancel </button>
