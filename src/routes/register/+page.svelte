649ce25b99087446cbac646695a68cc2158a1b19
jma <58450502+Y0URD34TH@users.noreply.github.com>
2025-05-01 08:30:03 -0300

    Update +page.svelte

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -2,7 +2,6 @@
 	import { goto } from '$app/navigation'; // Import goto
 	import { enhance } from '$app/forms';
 	import type { PageData, ActionData } from './$types';
-        import { POCKETBASE_SERVER } from '$env/static/private';
 
 	// import { loginWithEmail, authStore } from '$lib/pocketbase';
 	import '$lib/styles/login.scss';
@@ -12,7 +11,7 @@
 	let formElement: HTMLFormElement | null = null;
 
 	async function ifureadinguaregay() {
-		const res = await fetch(`${POCKETBASE_SERVER}api/collections/users/auth-with-password`, {
+		const res = await fetch('https://gld.pockethost.io/api/collections/users/auth-with-password', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
