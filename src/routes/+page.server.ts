875fae4d45707eb3254fdef22e0bfa2e225956d9
jma <58450502+Y0URD34TH@users.noreply.github.com>
2025-05-02 10:14:19 -0300

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,13 @@
 import { redirect } from '@sveltejs/kit'
 
 export const load = async ({ locals }) => {
+    // Check if the user is not logged in or the session is invalid
     if (locals.user === undefined || !locals.pb.authStore.isValid) {
-        return redirect(303, "/login")
+        throw redirect(303, '/login');  // Redirect to login if not valid
     }
-}
+
+    // Return the user profile data to the page
+    return {
+        profile: locals.user
+    };
+};
