97045081b774a84c89643fb4c615e3c20051a286
jma <58450502+Y0URD34TH@users.noreply.github.com>
2025-05-02 10:32:04 -0300

    Update +layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,15 +1,22 @@
 import { getPfp } from '$lib/utils.js';
 
 export const load = async ({ locals }) => {
-	if (locals.user && locals.pb.authStore.isValid) {
-		let picUrl = await getPfp(locals.pb, locals.user.id, true);
+  if (locals.user && locals.pb.authStore.isValid) {
+    let picUrl = await getPfp(locals.pb, locals.user.id, true);
 
-		if (picUrl === '')
-			picUrl = `https://api.dicebear.com/9.x/identicon/svg?seed=${locals.user.username}&backgroundColor=ffdfbf,b6e3f4`;
+    if (picUrl === '') {
+      picUrl = `https://api.dicebear.com/9.x/identicon/svg?seed=${locals.user.username}&backgroundColor=ffdfbf,b6e3f4`;
+    }
 
-		return {
-			profile: structuredClone(locals.user),
-			pfp: picUrl
-		};
-	}
+    console.log('User Data:', locals.user);
+    console.log('Username:', locals.user.username);
+
+    return {
+      profile: structuredClone(locals.user), 
+      pfp: picUrl
+    };
+  } else {
+    console.error("User is not authenticated or invalid.");
+    return { profile: null, pfp: null };
+  }
 };
